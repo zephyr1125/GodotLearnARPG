1de3c2cf51ebfe2f77243a793805dccca8a858f3
zephyr1125@gmail.com <theAnsweris42!>
2022-11-15 20:51:47 +0800

    part 3 done

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 8
-const MAX_SPEED = 120
-const FRICTION = 8
+const ACCELERATION = 200	# velocity per sec
+const MAX_SPEED = 100		# pixels per sec
+const FRICTION = 200		# velocity per sec
 
 var velocity = Vector2.ZERO
 
@@ -10,11 +10,11 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector.normalized() * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity) # velocity重新赋值可以让速度更新为碰撞后的新结果，以避免粘在碰撞边界的问题
